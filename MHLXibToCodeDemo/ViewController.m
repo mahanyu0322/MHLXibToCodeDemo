f1c37e8ac345ff16e204e854b3cc7c3f5885fa92
马洪亮 <656691922@qq.com>
2018-01-11 20:32:14 +0800

    修改代码

--- MHLXibToCodeDemo/ViewController.m
+++ MHLXibToCodeDemo/ViewController.m
@@ -33,22 +33,22 @@
     
     [super viewDidLoad];
     
-    //层级：1，父视图：UIView 的UITableView
-    UITableView *firstTableViewInOne                         =[[UITableView alloc]initWithFrame:CGRectMake(7.00f, 20.00f, 360.00f,502.00f) style:UITableViewStylePlain];
-    firstTableViewInOne.rowHeight                            =-1.00f;
-    firstTableViewInOne.delegate                           =self;
-    firstTableViewInOne.dataSource                         =self;
-    [self.view addSubview:firstTableViewInOne];
+//    //层级：1，父视图：UIView 的UITableView
+//    UITableView *firstTableViewInOne                         =[[UITableView alloc]initWithFrame:CGRectMake(7.00f, 20.00f, 360.00f,502.00f) style:UITableViewStylePlain];
+//    firstTableViewInOne.rowHeight                            =-1.00f;
+//    firstTableViewInOne.delegate                           =self;
+//    firstTableViewInOne.dataSource                         =self;
+//    [self.view addSubview:firstTableViewInOne];
+//    
+//    if (@available(iOS 11.0, *)) {
+//        firstTableViewInOne.contentInsetAdjustmentBehavior       =UIScrollViewContentInsetAdjustmentNever;
+//    } else {
+//        self.automaticallyAdjustsScrollViewInsets = NO;
+//    }
+//    
+//    [firstTableViewInOne registerNib:[UINib nibWithNibName:@"TastTableViewCell" bundle:nil] forCellReuseIdentifier:@"CELL"];
     
-    if (@available(iOS 11.0, *)) {
-        firstTableViewInOne.contentInsetAdjustmentBehavior       =UIScrollViewContentInsetAdjustmentNever;
-    } else {
-        self.automaticallyAdjustsScrollViewInsets = NO;
-    }
-    
-    [firstTableViewInOne registerNib:[UINib nibWithNibName:@"TastTableViewCell" bundle:nil] forCellReuseIdentifier:@"CELL"];
-    
-    [CodeGenerationEntry codeGenerationEntryfromClass:self automaticGenerationHierarchy:YES];
+    [CodeGenerationEntry codeGenerationEntryfromClass:self automaticGenerationHierarchy:NO];
     
 }
 
